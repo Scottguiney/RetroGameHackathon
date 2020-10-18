b2430dd424ee6c7d743ab838d8916de9f4bcf985
Scott Guiney <scott.guiney@outlook.com.au>
2020-10-18 12:03:49 +1100

    added basic character movement, mine logic and a game manager to handle game over

---
+++ Hackathon Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+
+{
+
+    private bool GameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameOver)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+
+
+    public void SetGameOver(bool State)
+    {
+        this.GameOver = State;
+    }
+
+}
---
+++ Hackathon Game/Assets/Scripts/MineScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject gameManager;
+    GameManager manager;
+    
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        manager = gameManager.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this);
+            manager.SetGameOver(true);
+        }
+        else if (collision.gameObject.tag == "Civillian")
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(this);
+    }
+}
