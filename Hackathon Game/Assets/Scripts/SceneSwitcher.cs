d68ee1da60eb4cc61488a287576c585e84cf04a4
tobyamielswin <102376266@student.swin.edu.au>
2020-10-18 14:34:47 +1100

    scene switcher

---
+++ Hackathon Game/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Update is called once per frame
+    public void GoToMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
